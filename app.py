e06e0784548a37c68da672123c4ea00d6012b815
onenp <iamonedev@gmail.com>
2017-07-15 20:04:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-"""
-#from __future__ import print_function
-#from future.standard_library import install_aliases
-#install_aliases()
+#!/usr/bin/env python
+
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
 
 from urllib.parse import urlparse, urlencode
 from urllib.request import urlopen, Request
